b1833ae1ec963cbd2a1ec8895276738ebffe9f20
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-10-31 11:43:04 -0300

    feat(lib): add JSDoc comments

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,22 @@
+/**
+ * @method factoryItem
+ * @param  {String} week
+ * @param  {Number} value
+ * @return {Object}       { week, value }
+ */
 const factoryItem = (week, value) => ({
   week,
   value
 })
 
+/**
+ * @method execute
+ * @param  {Array} array
+ * @param  {Number} value
+ * @param  {Number} times
+ * @param  {Number} initial
+ * @return {Array}
+ */
 const generate = (array = [], value = 2, times = 1, initial = 53) => {
   if (times === initial) {
     return array
